1c206ca10eb8c3d56b0895004b1b9c6137f09ba5
Matheus Klein <125155607+MatheusKlein@users.noreply.github.com>
2023-10-05 09:37:34 -0300

    Delete atv directory

--- atv/script.js
+++
@@ -1,9 +0,0 @@
-async function button1(){
-  window.location.href = "https://pt.wikipedia.org/wiki/Esta%C3%A7%C3%A3o_Parmentier";
-}
-async function button2(){
-  window.location.href = "https://pt.wikipedia.org/wiki/Guion_(Arkansas)";
-}
-async function button3(){
-  window.location.href = "https://pt.wikipedia.org/wiki/Fernando_de_Brito";
-}
